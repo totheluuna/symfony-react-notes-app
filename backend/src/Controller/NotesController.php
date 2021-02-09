9c694875a1af48532fd83caf812080d0e545bc4b
Luna Dilangalen <dadilangalen@gmail.com>
2021-02-09 22:08:05 +0800

    implemented fetch functionality

--- backend/src/Controller/NotesController.php
+++ backend/src/Controller/NotesController.php
@@ -6,41 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
 use App\Entity\Note;
 /**
  * @Route("/notes")
  */
 class NotesController extends AbstractController {
-    private const NOTES = [
-        [
-            'id' => 1,
-            'slug' => 'hello-world',
-            'title' => 'Hello world!'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'another-post',
-            'title' => 'This is another post!'
-        ],
-        [
-            'id' => 3,
-            'slug' => 'last-example',
-            'title' => 'This is the last example'
-        ],
-    ];
     /**
      * @Route("/{page}", name="note_list", defaults={"page": 5}, requirements={"page"="\d+"})
      */
     public function list($page = 1, Request $request) {
         $limit = $request->get('limit', 10);
+        $repository = $this->getDoctrine()->getRepository(Note::class);
+        $items = $repository->findAll();
 
         return $this->json(
             [
                 'page' => $page,
                 'limit' => $limit,
-                'data' => array_map(function ($item) {
-                    return $this->generateUrl('note_by_slug', ['slug' => $item['slug']]);
-                }, self::NOTES)
+                'data' => array_map(function (Note $item) {
+                    return $this->generateUrl('note_by_id', ['id' => $item->getId()]);
+                }, $items)
             ]
         );
     }
@@ -48,18 +34,9 @@ class NotesController extends AbstractController {
     /**
      * @Route("/note/{id}", name="note_by_id", requirements={"id"="\d+"})
      */
-    public function post($id) {
+    public function note($id) {
         return $this->json(
-            self::NOTES[array_search($id, array_column(self::NOTES, 'id'))]
-        );
-    }
-
-    /**
-     * @Route("/note/{slug}", name="note_by_slug")
-     */
-    public function postBySlug($slug) {
-        return $this->json(
-            self::NOTES[array_search($slug, array_column(self::NOTES, 'slug'))]
+            $this->getDoctrine()->getRepository(Note::class)->find($id)
         );
     }
 
